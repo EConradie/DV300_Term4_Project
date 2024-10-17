85129d5bf8120b8481cba79b74a9c1cb0a4f11d4
Erik <113360820+EConradie@users.noreply.github.com>
2024-10-17 07:54:05 +0200

    Create firebase.ts

---
+++ config/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "terpreter-818c9.firebaseapp.com",
+  projectId: "terpreter-818c9",
+  storageBucket: "terpreter-818c9.appspot.com",
+  messagingSenderId: "1079253722746",
+  appId: "1:1079253722746:web:c20400bf14bd9955c53dc3",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
